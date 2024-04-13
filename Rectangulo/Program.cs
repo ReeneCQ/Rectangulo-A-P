53de84fc4d1bff7f29bcf897212e724c3f1ce142
Reene <reenecondori0212@gmail.com>
2024-04-13 18:08:31 -0500

    Agregar archivos de proyecto.

---
+++ Rectangulo/Program.cs
@@ -0,0 +1,23 @@
+﻿
+
+using codigo_tecsup;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Creamos un objeto Rectangulo con las coordenadas de sus vertices
+        Rectangulo rectangulo = new Rectangulo(0, 0, 0, 3, 4, 3, 4, 0);
+
+        //Podemos cambiar los datos de la coordenada
+        rectangulo.X1 = 2;
+
+
+        // Calculamos y mostramos el area y el perimetro del rectangulo
+        // Para mostrar debemos usar el objeto.Metodo
+        Console.WriteLine("Area del rectangulo: " + rectangulo.CalcularArea());
+        Console.WriteLine("Perímetro del rectangulo: " + rectangulo.CalcularPerimetro());
+        Console.WriteLine("Coordenada X4: " + rectangulo.X4);
+
+    }
+}
---
+++ Rectangulo/Rectangulo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace codigo_tecsup
+{
+    public class Rectangulo
+    {
+        // Definimos las propiedades para los vértices del rectangulo
+        public double X1 { get; set; }
+        public double Y1 { get; set; }
+        public double X2 { get; set; }
+        public double Y2 { get; set; }
+        public double X3 { get; set; }
+        public double Y3 { get; set; }
+        public double X4 { get; set; }
+        public double Y4 { get; set; }
+
+        // Constructor que recibe las coordenadas de los vertices
+        public Rectangulo(double x1, double y1, double x2, double y2, double x3, double y3, double x4, double y4)
+        {
+            X1 = x1;
+            Y1 = y1;
+            X2 = x2;
+            Y2 = y2;
+            X3 = x3;
+            Y3 = y3;
+            X4 = x4;
+            Y4 = y4;
+        }
+
+        // Metodo para calcular el area del rectangulo
+        public double CalcularArea()
+        {
+            // Calculamos la base y la altura del rectangulo
+            double baseRectangulo = Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2));
+            double alturaRectangulo = Math.Sqrt(Math.Pow(X3 - X2, 2) + Math.Pow(Y3 - Y2, 2));
+
+            // El area del rectangulo es la base por la altura
+            double areaRectangulo = baseRectangulo * alturaRectangulo;
+            return areaRectangulo;
+        }
+
+        // Metodo para calcular el perimetro del rectangulo
+        public double CalcularPerimetro()
+        {
+            // Calculamos la distancia entre los vertices del rectangulo
+            double lado1 = Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2));
+            double lado2 = Math.Sqrt(Math.Pow(X3 - X2, 2) + Math.Pow(Y3 - Y2, 2));
+
+            // El perimetro es la suma de todos los lados
+            double perimetroRectangulo = 2 * (lado1 + lado2);
+            return perimetroRectangulo;
+        }
+    }
+}
